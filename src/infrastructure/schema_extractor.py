c9d0322a050cf8f2082e49408b2e993900371f8d
kkosyan <lizhenina.k@gmail.com>
2023-09-10 21:53:06 +0300

    finalize schema extractor

--- src/infrastructure/schema_extractor.py
+++ src/infrastructure/schema_extractor.py
@@ -1,11 +1,29 @@
+import re
+import subprocess
+
 from src.domain.schema_extractor import SchemaExtractor
-from src.domain.database import Database
 
 
 class PostgresSchemaExtractor(SchemaExtractor):
 
-    def __init__(self, database: Database):
-        self._database = database
+    def __init__(self, host: str, port: str, database: str, user: str, password: str):
+        self._host = host
+        self._port = port
+        self._dbname = database
+        self._user = user
+        self._password = password
 
-    def extract(self, query: str) -> str:
-        ...
+    def extract(self, table: str) -> str:
+        pg_dump_proc = subprocess.Popen(
+            ['pg_dump',
+             f"--dbname=postgresql://{self._user}:{self._password}@{self._host}:{self._port}/{self._dbname}",
+             "--schema-only",
+             "-t",
+             f"{table}",
+             ],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+        output, err = pg_dump_proc.communicate()
+
+        return re.search("Schema: \w+", output.decode()).group().split()[1]
