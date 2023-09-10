0fd3fdafa3d50b8e3bcd6bd3da94fc1f18a1c475
kkosyan <lizhenina.k@gmail.com>
2023-09-10 23:08:20 +0300

    rename domain object

---
+++ src/domain/extractors.py
@@ -0,0 +1,11 @@
+import abc
+
+
+class DdlExtractor(abc.ABC):
+    @abc.abstractmethod
+    def extract_schema(self, table: str) -> str:
+        ...
+
+    @abc.abstractmethod()
+    def extract_objects(self, table: str) -> str:
+        ...
--- src/domain/schema_extractor.py
+++
@@ -1,7 +0,0 @@
-import abc
-
-
-class SchemaExtractor(abc.ABC):
-    @abc.abstractmethod
-    def extract(self, query: str) -> str:
-        ...
