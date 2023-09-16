dd377142e1f2d4442bdc68b1d44cc43ebe4ff338
kkosyan <lizhenina.k@gmail.com>
2023-09-16 23:19:51 +0300

    fix data generator

--- src/infrastructure/sdv_data_generator.py
+++ src/infrastructure/sdv_data_generator.py
@@ -1,7 +1,8 @@
 from typing import Optional
 
 import pandas as pd
-from sdv.tabular import GaussianCopula
+from sdv.metadata import SingleTableMetadata
+from sdv.single_table import GaussianCopulaSynthesizer
 
 from src.domain.data_generator import DataGenerator, DataGeneratorError
 from src.domain.database import Database
@@ -33,8 +34,11 @@ class SdvDataGenerator(DataGenerator):
     def generate(self, table: str, sample_limit: Optional[int] = 5_000, synthetic_limit: Optional[int] = 5_000):
         try:
             data = self._extract_sample_data(table=table, sample_limit=sample_limit)
-            model = GaussianCopula()
+            metadata = SingleTableMetadata()
+            metadata.detect_from_dataframe(data=data)
+            model = GaussianCopulaSynthesizer(metadata=metadata)
             model.fit(data)
-            return model.sample(synthetic_limit).to_records(index=False)
+            synthetic_data = model.sample(synthetic_limit)
+            return [list(row) for row in synthetic_data.itertuples(index=False)]
         except Exception as e:
             DataGeneratorError(f"Cannot generate data for table {table} with exception {e}")
