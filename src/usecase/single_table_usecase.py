56f177249d1f915660f5e95f62ce1367a0176a45
kkosyan <lizhenina.k@gmail.com>
2023-09-16 23:19:29 +0300

    fix usecase

--- src/usecase/single_table_usecase.py
+++ src/usecase/single_table_usecase.py
@@ -1,16 +1,14 @@
 import logging
 
 from src.domain.data_generator import DataGenerator
-from src.domain.database import Database
 from src.domain.ddl_extractor import DdlExtractor
 from src.domain.exception import DataEmulatorBaseException
 from src.domain.synthetic_data_uploader import SyntheticDataUploader
 
 
 class DataEmulatorForSingleTable:
-    def __init__(self, database: Database, synthetic_data_uploader: SyntheticDataUploader,
+    def __init__(self, synthetic_data_uploader: SyntheticDataUploader,
                  data_generator: DataGenerator, ddl_extractor: DdlExtractor):
-        self._database = database
         self._synthetic_data_uploader = synthetic_data_uploader
         self._data_generator = data_generator
         self._ddl_extractor = ddl_extractor
@@ -20,7 +18,7 @@ class DataEmulatorForSingleTable:
     def _execute(self, table: str):
         schema_ddl = self._ddl_extractor.extract_schema(table=table)
         objects_ddl = self._ddl_extractor.extract_objects(table=table)
-        ddl_query = ' '.join([schema_ddl, objects_ddl])
+        ddl_query = schema_ddl + objects_ddl
 
         synthetic_data = self._data_generator.generate(table=table)
         self._synthetic_data_uploader.apply_ddl(ddl_query=ddl_query)
